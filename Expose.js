81b85eda2287a3ee8dbb8d0e5847d0b1c5c207a4
Graham Reeves <graham@grahamreeves.com>
2019-11-28 19:53:34 +0000

    Initial version opens a broadcast socket and a listening socket

---
+++ Expose.js
@@ -0,0 +1,5 @@
+
+//	namespace for Expose network API
+const Expose = {};
+Expose.BroadcastPorts = [9000];
+Expose.ListenPorts = [9001, 9002, 9003, 9004, 9005];
--- bootup.js
+++ bootup.js
@@ -1,18 +1,32 @@
-//	replace this with a proper mini unit test/api example set of scripts
-Pop.Debug("Virtual Reality Test");
-
-
-const Window = new Pop.Opengl.Window("VR!");
-
-Window.OnRender = function (RenderTarget)
+Pop.Include = function (Filename)
 {
+	const Source = Pop.LoadFileAsString(Filename);
+	return Pop.CompileAndRun(Source,Filename);
+}
+Pop.Include('Expose.js');
+
+
+//	we need some render context for openvr
+const Window = new Pop.Opengl.Window("Render Context");
+Window.OnRender = function (RenderTarget) {
 	RenderTarget.ClearColour(0, 1, 1);
 }
-//	find devices
-//	attach to device
-//	get updates
-//	render
-let Hmd = new Pop.Openvr.Hmd("Device Name", Window);
+
+
+//	queue of outputs to send
+const OutputPoses = [];
+
+function OnOutputPose(Pose)
+{
+	OutputPoses.push(Pose);
+	//	wake up output thread
+}
+
+
+
+//	create openvr overlay
+const IsOverlay = false;
+const Hmd = new Pop.Openvr.Hmd("Device Name",Window);
 
 Hmd.OnPoses = function (Poses)
 {
@@ -22,8 +36,10 @@ Hmd.OnPoses = function (Poses)
 			return;
 		Pop.Debug(`Device ${Index} connected; valid=${Pose.IsValidPose}`);
 	}
-	Poses.forEach(EnumPose);
+	//Poses.forEach(EnumPose);
 	//Pop.Debug("New JS Poses x" + Poses.length);
+
+	//	turn this into 
 }
 
 Hmd.OnRender = function(RenderTarget,Camera)
@@ -35,3 +51,83 @@ Hmd.OnRender = function(RenderTarget,Camera)
 	else
 		RenderTarget.ClearColour( 0,0,1 );
 }
+
+
+
+function OnBroadcastMessage(Message,Socket)
+{
+	//	send back our address
+	Pop.Debug("Got broadcast message",JSON.stringify(Message));
+	Socket.Send(Message.Peer,"Hello");
+}
+
+
+function OnRecievedMessage(Message,Socket)
+{
+	//	send to expose api for decoding
+	Pop.Debug("Got message",JSON.stringify(Message));
+}
+
+//	create discovery udp
+async function RunBroadcast(OnMessage)
+{
+	while (true)
+	{
+		try
+		{
+			const Socket = new Pop.Socket.UdpBroadcastServer(Expose.BroadcastPorts[0]);
+			Pop.Debug("Broadcast listening on ",JSON.stringify(Socket.GetAddress()));
+
+			while (true)
+			{
+				Pop.Debug("Waiting for message");
+				const Message = await Socket.WaitForMessage();
+				OnMessage(Message,Socket);
+			}
+		}
+		catch (e)
+		{
+			Pop.Debug("Exception in broadcast loop: " + e);
+			await Pop.Yield(2000);
+		}
+	}
+}
+
+
+
+//	keep trying to run servers
+async function RunServer(OnMessage)
+{
+	let PortIndex = 0;
+	function GetPortIndex()
+	{
+		const PortCount = Expose.ListenPorts.length;
+		PortIndex = (PortIndex + 1) % PortCount;
+		return Expose.ListenPorts[PortIndex];
+	}
+
+	while (true)
+	{
+		try
+		{
+			const Port = GetPortIndex();
+			const Socket = new Pop.Websocket.Server(Port);
+			Pop.Debug("Websocket listening on ",JSON.stringify(Socket.GetAddress()));
+
+			while (true)
+			{
+				const Message = await Socket.WaitForMessage();
+				OnMessage(Message,Socket);
+			}
+		}
+		catch (e)
+		{
+			Pop.Debug("Exception in server loop: " + e);
+			await Pop.Yield(2000);
+		}
+	}
+}
+
+
+RunBroadcast(OnBroadcastMessage).then(Pop.Debug).catch(Pop.Debug);
+RunServer(OnRecievedMessage).then(Pop.Debug).catch(Pop.Debug);
